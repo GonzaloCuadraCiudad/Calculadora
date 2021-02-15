e11b87cf2b81955b2555d14e5f89f36d88365ce9
GonzaloCuadraCiudad <gonzaloesic@gmail.com>
2021-02-15 22:13:36 +0100

    Prueba2

---
+++ sucio2/src/sucio2/Pruebadetest.java
@@ -0,0 +1,12 @@
+package sucio2;
+
+public class Pruebadetest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola, esto es una prueba");
+System.out.println("haciendo prueba");
+	}
+
+}
+
+
